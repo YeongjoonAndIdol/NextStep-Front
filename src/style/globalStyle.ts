532af5aa114183aa96342851bef4bdd11e918cc9
Yeong joon <bluehome8626@naver.com>
2022-09-05 01:49:22 +0900

    chore: fonts setting

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -5,7 +5,11 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-tap-highlight-color:rgba(255,255,255,0);
     user-select: none;
     -webkit-touch-callout: none;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    font-family: SF-Pro,-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+  @font-face {
+    font-family: SF-Pro;
+    src: url('../public/fonts/SF-Pro-Display-Semibold.otf') format("truetype");
   }
 `;
