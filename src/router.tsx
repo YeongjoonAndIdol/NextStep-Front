351c5d96f48f32c2d107b6fabf6e97f94c84c22c
Yeong joon <bluehome8626@naver.com>
2022-09-18 16:28:42 +0900

    chore: ranking page setting

--- src/router.tsx
+++ src/router.tsx
@@ -3,16 +3,18 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import PageMyPage from "./pages/mypage/PageMyPage";
 import QuestListPage from "./pages/quest/QuestListPage";
 import RecommenListPage from "./pages/quest/RecommenListPage";
+import RankingPage from "./pages/ranking/RankingPage";
 import SearchPage from "./pages/search/SearchPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/myquest" element={<QuestListPage />} />
-        <Route path="/recommen" element={<RecommenListPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/mypage" element={<PageMyPage />} />
+        <Route path='/myquest' element={<QuestListPage />} />
+        <Route path='/recommen' element={<RecommenListPage />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/mypage' element={<PageMyPage />} />
+        <Route path='/ranking' element={<RankingPage />} />
       </Routes>
     </BrowserRouter>
   );
