81d01126fdf0ff8a1bbe122e91784f4bce513caa
Yeong joon <bluehome8626@naver.com>
2022-09-19 17:10:32 +0900

    feat: theme color 수정

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -21,7 +21,7 @@ export const theme = {
     gray_color2: "#EDEDED",
     gray_color3: "#B7B7B7",
     gray_color4: "#D9D9D9",
-    gray_color5: "#F5F5F5",
+    gray_color5: "#F4F4F4",
 
     error_color: "#FF6464",
   },
