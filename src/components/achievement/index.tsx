7c454af65255f843f551b9674c8d4e498a140ad4
Yeong joon <bluehome8626@naver.com>
2022-10-11 15:49:17 +0900

    feat: title 추가

--- src/components/achievement/index.tsx
+++ src/components/achievement/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import leftArrow from "../../assets/imgs/common/leftArrow.svg";
 import recall from "../../assets/imgs/Achievement/recall.svg";
 import Week from "./Week";
+import AchievementBox from "./AchievementBox";
 
 const Achievement = () => {
   return (
@@ -15,6 +16,12 @@ const Achievement = () => {
       <WeekDiv>
         <Week />
       </WeekDiv>
+      <TitleDiv>
+        <p>서울대생의 하루 루틴</p>
+      </TitleDiv>
+      <LogDiv>
+        <AchievementBox />
+      </LogDiv>
     </Wrapper>
   );
 };
@@ -37,8 +44,21 @@ const Hedaer = styled.div`
 
 const WeekDiv = styled.div`
   width: 342px;
+  padding-bottom: 38px;
   display: flex;
   justify-content: space-between;
 `;
+
+const TitleDiv = styled.div`
+  padding-bottom: 20px;
+  > p {
+    margin: 0;
+    font-weight: 590;
+    font-size: ${({ theme }) => theme.font_size.SF_Pro16};
+    color: ${({ theme }) => theme.color.main};
+  }
+`;
+
+const LogDiv = styled.div``;
 
 export default Achievement;
