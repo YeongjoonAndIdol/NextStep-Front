40d3f658c4c73b40403ae08e16655e86bb7d3a80
Yeong joon <bluehome8626@naver.com>
2022-10-12 09:02:21 +0900

    feat: 성취도 박스 퍼블리싱

--- src/components/achievement/AchievementBox.tsx
+++ src/components/achievement/AchievementBox.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { Category } from "../../interface/category";
+import book from "../../assets/imgs/Achievement/book.svg";
+import brain from "../../assets/imgs/Achievement/brain.svg";
+import health from "../../assets/imgs/Achievement/health.svg";
+import mountain from "../../assets/imgs/Achievement/mountain.svg";
+import pencil from "../../assets/imgs/Achievement/pencil.svg";
+import watch from "../../assets/imgs/Achievement/watch.svg";
 
 const colorState = {
   SPORT: "#FFB55D",
@@ -11,12 +17,96 @@ const colorState = {
   ENVIRONMENT: "#88FFDB",
 } as const;
 
-const boxColor = colorState["STUDY"];
+const imgState = {
+  SPORT: `${health}`,
+  STUDY: `${brain}`,
+  LIFE: `${watch}`,
+  HOBBY: `${pencil}`,
+  BOOK: `${book}`,
+  ENVIRONMENT: `${mountain}`,
+} as const;
 
 const AchievementBox = () => {
-  return <></>;
+  const boxColor = colorState["STUDY"];
+  const boxImg = imgState["STUDY"];
+
+  return (
+    <Wrapper>
+      <LeftBar boxColor={boxColor} />
+      <ContentDiv>
+        <ContentHeader>
+          <img src={boxImg} alt='icon' />
+          <p>공부</p>
+        </ContentHeader>
+        <TitleDiv>
+          <p>수학 문제 풀기</p>
+        </TitleDiv>
+        <Content>
+          <p>
+            기본적으로 수학에 대한 지식이 풍부해지고, 모자란 창의성과 유연한
+            사고, 다양성과 순발력을 키워주는 효과가 강합니다.
+          </p>
+        </Content>
+        <ExpDiv>
+          <p>+ 540 EXP</p>
+        </ExpDiv>
+      </ContentDiv>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const LeftBar = styled.div<{ boxColor: string }>`
+  width: 4px;
+  height: 120px;
+  background-color: ${({ boxColor }) => boxColor};
+`;
+
+const ContentDiv = styled.div``;
+
+const ContentHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 19px;
+  padding-top: 5px;
+  padding-bottom: 12px;
+  > p {
+    margin: 0;
+    font-weight: 590;
+    font-size: ${({ theme }) => theme.font_size.SF_Pro12};
+  }
+`;
+
+const TitleDiv = styled.div`
+  padding-bottom: 4px;
+  > p {
+    margin: 0;
+    font-weight: 590;
+    font-size: ${({ theme }) => theme.font_size.SF_Pro14};
+  }
+`;
+
+const Content = styled.div`
+  padding-bottom: 4px;
+  > p {
+    margin: 0;
+    width: 354px;
+    height: 38px;
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.font_size.SF_Pro12};
+  }
+`;
+
+const ExpDiv = styled.div`
+  > p {
+    margin: 0;
+    font-weight: 590;
+    font-size: ${({ theme }) => theme.font_size.SF_Pro10};
+  }
+`;
 
 export default AchievementBox;
